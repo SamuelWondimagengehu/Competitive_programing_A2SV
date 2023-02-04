e8b12adfc67f9b29cfc177afab71b793f1633e45
SamuelWondimagengehu <99725255+SamuelWondimagengehu@users.noreply.github.com>
2023-02-03 21:31:45 -0800

    Create isValid.java

---
+++ isValid.java
@@ -0,0 +1,31 @@
+class Solution {
+    public char match(String r) {
+		char c = 'r';
+		switch(r) {
+		case "(": c = ')';
+			break;
+		case "{": c = '}';
+			break;
+		case "[": c = ']';
+			break;
+		}
+		return c;
+	}
+	
+    public boolean isValid(String s) {
+        Stack<String> stack = new Stack();
+
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c + "");
+            } 
+            else if(!stack.isEmpty()) {
+                if(match(stack.peek()) == c) stack.pop();
+                else return false;
+            }
+            else return false;
+        }        
+
+        return stack.isEmpty();
+    }
+}
