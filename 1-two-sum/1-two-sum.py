b0ad964d297709e70af826f9c641b5d48e28d4fb
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2023-05-26 21:24:03 +0300

    Time: 80 ms (49.99%), Space: 17.6 MB (16.67%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        book = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in book:
+                return [book[complement],i]
+            else:
+                book[nums[i]] = i
+                
+                # 3 : 0, 4 : 1, 2 : 2 
