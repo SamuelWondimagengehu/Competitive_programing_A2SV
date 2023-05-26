8d29fa2327eaa9467cdad8b145857c65b45799a6
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2023-05-26 21:26:38 +0300

    Time: 72 ms (64.57%), Space: 17.8 MB (8.49%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        book = {}
+        book = defaultdict(int)
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in book:
