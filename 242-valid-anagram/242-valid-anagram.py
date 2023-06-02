39e815c49f507261600c687187f5f89fa512cd30
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2023-06-02 18:06:50 +0300

    Time: 50 ms (86.07%), Space: 16.8 MB (41.46%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -3,5 +3,6 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        if Counter(s) == Counter(t):
+        if Counter(s) == Counter(t): # a:3,n:1,g:1,r:1,m:1
             return True
+        
