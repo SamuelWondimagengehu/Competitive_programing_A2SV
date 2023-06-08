62200fda5988324c9db13297d1683121480d6e6c
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2023-06-08 21:28:32 +0300

    Time: 1430 ms (28.42%), Space: 72.8 MB (8.74%) - LeetHub

---
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        positions = {}
+        
+        for i in range(len(nums)):
+            positions[nums[i]] = i
+        
+        for i, j in operations:
+            if i in positions.keys():
+                index = positions[i]
+                nums[index] = j
+                positions[nums[index]] = index
+        return nums
