3b125555d999ca0306b1be49120358d8732e325f
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2023-07-01 18:01:25 +0300

    Time: 173 ms (78.41%), Space: 18 MB (52.40%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        seeker, placeholder = 0, 0
+        
+        while seeker < len(nums):
+            if nums[seeker] != 0:
+                nums[placeholder], nums[seeker] = nums[seeker], nums[placeholder]
+                placeholder += 1
+            seeker += 1
+            
