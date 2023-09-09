0427eaa12fc947d86fe3854b9fa7b689e0a16462
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2023-09-09 20:00:54 +0300

    Time: 43 ms (94.61%), Space: 42.3 MB (14.09%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let res_arr = []
+    for(let i = 0; i < arr.length; i++) {
+        res_arr[i] = fn(arr[i], i);
+    }
+    return res_arr;
+};
