82ad445d167017a95210c44e4df2f00688cf50f5
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2023-12-07 11:06:12 +0300

    Added solution for Max Consecutive Ones

--- leet-code-solutons/max-consecutive-ones.py
+++ leet-code-solutons/max-consecutive-ones.py
@@ -1,7 +1,6 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        #use a hashmap to keep track of the index's of the ones
-        #if current index is imediately next and current number is one increase count
+        
         book = defaultdict(int)
         for i, num in enumerate(nums):
             if i == 0 and num == 1:
