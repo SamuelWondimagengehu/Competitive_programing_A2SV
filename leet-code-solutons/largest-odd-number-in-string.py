be1595b7f97678cda10d4b75d267d9a1a348573d
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2023-12-13 11:18:50 +0300

    Added solution for Largest Odd Number in String

--- leet-code-solutons/largest-odd-number-in-string.py
+++ leet-code-solutons/largest-odd-number-in-string.py
@@ -3,14 +3,8 @@ class Solution:
         ans = ""
         
         for i in range(len(num) - 1, -1, -1):
-            if len(ans) == 0 and int(num[i]) % 2 != 0:
-                ans += num[i]
-            elif len(ans) >= 1 and int(num[i]) % 2 == 0:
-                ans += num[i]
-            elif int(num[i]) % 2 != 0:
-                ans += num[i]   
-            else:
-                continue
-                
-        return ans[::-1]
+            if int(num[i]) % 2 != 0:
+                return num[:i+1]
+            
+        return ""
         
