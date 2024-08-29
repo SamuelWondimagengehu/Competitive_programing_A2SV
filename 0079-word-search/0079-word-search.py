1920978455b8f359bf20185095bd7e4324e98d72
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2024-08-29 16:31:48 +0300

    Time: 4019 ms (60.28%), Space: 16.7 MB (28.57%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -4,34 +4,37 @@ class Solution:
         path = set()
         
         def dfs(r, c, i):
-            if i == len(word): #If i reaches the last character of word
+            #If word pointer manages to reach the end without 
+            #the conditions failing in the middle: return True
+            if i == len(word): 
                 return True
             
-            if(
-                r < 0 or c < 0
-               or r >= ROWS or c >= COLS 
-               or word[i] != board[r][c] 
-               or (r, c) in path
-              ):
-                
+            #if the board pointers go out of bound, or word[i] not equals board[r][c]
+            #or we're already on a path that has been visited: return False
+            if (
+                r < 0 or c < 0 or
+                r >= ROWS or c >= COLS or
+                word[i] != board[r][c] or
+                (r, c) in path
+            ):
                 return False
             
             path.add((r, c))
             
+            #explore in all four directions
             res = (
-                    
-                    dfs(r + 1, c, i + 1) or
-                    dfs(r - 1, c, i + 1) or
-                    dfs(r, c + 1, i + 1) or 
-                    dfs(r, c - 1, i + 1) 
-                  )
+                dfs(r + 1, c, i + 1) or
+                dfs(r - 1, c, i + 1) or
+                dfs(r, c + 1, i + 1) or
+                dfs(r, c - 1, i + 1)
+            )
             
-            path.remove((r,c))
+            path.remove((r, c))
             
             return res
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if dfs(r, c, 0): return True
+        for i in range(ROWS):
+            for j in range(COLS):
+                if dfs(i, j, 0): return True
         
         return False
