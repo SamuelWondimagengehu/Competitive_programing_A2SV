3b01613636f074687e5e0f26cb05b1853e8bbf68
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2024-11-28 10:44:08 +0300

    Time: 3 ms (6.56%), Space: 51.7 MB (6.42%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.ts
@@ -0,0 +1,14 @@
+function convertToTitle(columnNumber: number): string {
+    const alphabets: string[] = Array.from({length: 26}, (_, i) => String.fromCharCode(65 + i));
+    let res: string = "";
+    
+    while(columnNumber > 0) {
+        columnNumber -= 1;
+        console.log(columnNumber);
+        res = alphabets[columnNumber % 26] + res;
+        
+        columnNumber= Math.floor(columnNumber / 26);
+    }
+  
+    return res;
+};
