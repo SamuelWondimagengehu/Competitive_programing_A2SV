c37550cfa59796a768646369628e3d0c8b2c6216
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2024-11-28 09:46:53 +0300

    Time: 0 ms (100.00%), Space: 51.7 MB (13.10%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.ts
+++ 0058-length-of-last-word/0058-length-of-last-word.ts
@@ -1,5 +1,3 @@
 function lengthOfLastWord(s: string): number {
-    let l =  s.split(" ").filter((val) => val.length > 0)
-    
-    return l.pop().length
+    return s.split(" ").filter((val) => val.length > 0).pop().length
 };
