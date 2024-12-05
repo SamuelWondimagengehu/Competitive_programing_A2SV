a24d1c0536c388f16549e85dfc401013cb95fc76
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2024-12-05 18:09:40 +0300

    Time: 0 ms (100.00%), Space: 17.3 MB (6.97%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -5,10 +5,10 @@ class Solution:
         elif n == 1: 
             return True
         elif n % 2 == 0: 
-            return self.isUgly(n//2)
+            return self.isUgly(n // 2)
         elif n % 3 == 0: 
-            return self.isUgly(n//3)
+            return self.isUgly(n // 3)
         elif n % 5 == 0: 
-            return self.isUgly(n//5)
+            return self.isUgly(n // 5)
         else: 
             return False
