af5dade0782fab91cd34c295c06c93c31860ebef
Samuel Wondimagegnehu <99725255+samwondim@users.noreply.github.com>
2024-12-06 11:04:37 +0300

    Time: 367 ms (10.39%), Space: 17.2 MB (14.26%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,18 +1,4 @@
 class Solution:
-    def isUgly(self, n):
-        if n < 1:
-            return False
-        elif n == 1:
-            return True
-        elif n % 2 == 0:
-            return self.isUgly(n // 2)
-        elif n % 3 == 0:
-            return self.isUgly(n // 3)
-        elif n % 5 == 0:
-            return self.isUgly(n // 5)
-        else:
-            return False
-
     def nthUglyNumber(self, n: int) -> int:
         ugly_nums = set()
         ugly_nums.add(1)
